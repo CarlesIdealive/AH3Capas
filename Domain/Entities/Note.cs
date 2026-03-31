2b08eb975e210747d95983a26b19fb7657ac32f2
Carles Labrana <carles.labrana@idealiveconsulting.com>
2026-03-31 20:42:46 +0200

    Añadir clase Note con validación de mensaje
    Se incorpora la clase Note en DomainComponent.Entities, que representa una nota vinculada a un elemento. Incluye validaciones para asegurar que el mensaje no esté vacío ni supere los 100 caracteres, lanzando excepciones en caso contrario.

---
+++ Domain/Entities/Note.cs
@@ -0,0 +1,36 @@
+﻿namespace DomainComponent.Entities
+{
+    public class Note
+    {
+
+        public int Id { get; set; }
+        public int ItemId { get; private set; }
+        public string Message { get; set; }
+
+        public Note(int id, int itemid, string message)
+        {
+            UpdateMessage(message);
+            Id = id;
+            ItemId = itemid;
+        }
+
+
+        public void UpdateMessage(string newMessage)
+        {
+            if (string.IsNullOrWhiteSpace(newMessage))
+            {
+                throw new ArgumentException("El mensaje no puede estar vacío.");
+            }
+            if (newMessage.Length > 100)
+            {
+                throw new ArgumentException("El mensaje no puede superar los 100 caracteres");
+            }
+            Message = newMessage;
+
+
+        }
+
+
+
+    }
+}
