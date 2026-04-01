3040e06cbfe4a59c25eaf0a6e2b4b488d56cb38b
Carles Labrana <carles.labrana@idealiveconsulting.com>
2026-04-01 15:18:54 +0200

    Añade soporte para entidad Note y endpoints asociados
    Se agregan servicios y repositorios genéricos para la entidad Note, así como los endpoints GET y POST /notes en la API. Además, se inicializa la propiedad CreatedDate en NoteModel con la fecha actual por defecto. Esto permite gestionar notas asociadas a los ítems de forma estructurada.

--- APIAppItems/Program.cs
+++ APIAppItems/Program.cs
@@ -1,5 +1,7 @@
 using ApplicationComponent;
 using Domain.Interfaces;
+using DomainComponent.Entities;
+using DomainComponent.Interfaces;
 using Infrastructure.Repository;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,7 +14,9 @@ builder.Services.AddOpenApi();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ItemsDbContext>(opt => opt.UseSqlServer(connectionString));
 builder.Services.AddTransient<IRepository, ItemRepository>();
+builder.Services.AddTransient<ICommonRepository<Note>, NoteRepository>();
 builder.Services.AddTransient<IService, ItemService>();
+builder.Services.AddTransient<ICommonService<Note>, NoteService>();
 
 
 builder.Services.AddEndpointsApiExplorer();
@@ -39,6 +43,12 @@ app.MapPost("/items", async (string title, IService service) => {
     return Results.Created();
 }).WithName("AddItem");
 
+app.MapGet("/notes", async (ICommonService<Note> service) => await service.GetAsync()).WithName("GetNotes");
+app.MapPost("/notes", async (int id, string message, int itemId, ICommonService<Note> service) => {
+    var note = new Note(id, itemId , message);
+    await service.AddAsync(note);
+    return Results.Created();
+}).WithName("AddNote");
 
 
 app.Run();
--- Infrastructure.Component/Models/NoteModel.cs
+++ Infrastructure.Component/Models/NoteModel.cs
@@ -9,7 +9,7 @@ namespace Infrastructure.Repository.Models
         public int Id { get; set; }
         public int ItemId { get; set; }
         public string Message { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
         public string Color { get; set; }
 
         public ItemModel Item { get; set; }
